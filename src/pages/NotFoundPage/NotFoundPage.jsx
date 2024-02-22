2e14444c4d3395041bcb84d107a83cbb0aa0f8cf
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-02-22 13:45:20 +0100

    Add Not Found page

---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+import not from '../../components/images/not.png';
+import { Container } from './NotFound.styled';
+
+const NotFound = () => {
+    return (
+        <Container>  
+            <img src={not} width={400}/>
+        </Container>
+        
+                                    
+    );
+};
+
+export default NotFound;

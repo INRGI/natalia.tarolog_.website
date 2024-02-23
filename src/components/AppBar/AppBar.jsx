9217bf02854b0d2bfb056cc577ba502f71f6a24e
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-02-23 15:11:43 +0100

    Add react icons

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 const AppBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (<header>
         <p>AppBar</p>
+        <button type="button" onClick={handleClick}><RxHamburgerMenu /></button>
+
+
+        {isOpen && (<div>
+            <p>sidebar</p>
+            <button>Click</button>
+        </div>)}
     </header>)
 }
 

44c426c1efcc96e2144623b8632f3b59415492f9
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-03-01 09:40:50 +0100

    Carousel styles

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,6 +1,7 @@
 import banner from '../../components/images/banner.jpeg'
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import { Image } from './Slider.styled';
 
 const handleDragStart = (e) => e.preventDefault();
 
@@ -14,17 +15,17 @@ const responsive = {
 };
 
 const items = [
-  <img key={1} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="1"/>,
-  <img key={2} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="2"/>,
-    <img key={3} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="3"/>,
-  <img key={4} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="4"/>,
-    <img key={5} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="5"/>,
-  <img key={6} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="6"/>,
+  <Image key={1} src={banner} onDragStart={handleDragStart} role="presentation" width={100}  data-value="1"/>,
+  <Image key={2} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="2"/>,
+    <Image key={3} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="3"/>,
+  <Image key={4} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="4"/>,
+    <Image key={5} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="5"/>,
+  <Image key={6} src={banner} onDragStart={handleDragStart} role="presentation" width={100} data-value="6"/>,
 ];
 
 const Slider = () => {
   return (
-      <AliceCarousel responsive={responsive} infinite={true} mouseTracking items={items} autoPlay={true} disableButtonsControls={true} disableDotsControls={true} />
+      <AliceCarousel autoWidth={true} responsive={responsive} infinite={true} mouseTracking items={items} autoPlay={true} disableButtonsControls={true} disableDotsControls={true} />
   );
 }
 
--- src/components/Slider/Slider.styled.jsx
+++ src/components/Slider/Slider.styled.jsx
@@ -4,3 +4,8 @@ import styled from "@emotion/styled";
 export const SliderContainer = styled.div`
     display: flex;
 `;
+
+export const Image = styled.img`
+    padding-left: 10px;
+    padding-right: 10px;
+`;

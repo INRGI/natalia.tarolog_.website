3547b9ca193d0b36f080ae6a69913b7de38c8f1f
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-03-04 10:12:31 +0100

    Modal styles

--- src/components/ModalCard/ModalCard.jsx
+++ src/components/ModalCard/ModalCard.jsx
@@ -1,4 +1,24 @@
 import Modal from 'react-modal';
+import { Img } from './ModalCard.styled';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        padding: '0',
+        background: 'transparent',
+        border: 'none',
+        margin: '0',
+    width: '70%'
+    },
+    overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.7)'
+    }
+};
 
 const ModalCard = ({ isOpen, onRequestClose, imageSrc }) => {
     return (
@@ -7,8 +27,9 @@ const ModalCard = ({ isOpen, onRequestClose, imageSrc }) => {
             onRequestClose={onRequestClose}
             ariaHideApp={false}
             contentLabel="Example Modal"
+            style={customStyles}
         >
-            <img src={imageSrc} />
+            <Img src={imageSrc} />
         </Modal>
     );
 };
---
+++ src/components/ModalCard/ModalCard.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+import Modal from 'react-modal';
+
+export const Img = styled.img`
+    max-width: 100%;
+`
+
+export const StyledModal = styled(Modal)`
+    
+`
